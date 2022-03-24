849047364ba0dda5467d7c4ac9b573086cbbe6e9
YanJun19820509 <mqsy_yj@163.com>
2022-03-24 19:02:33 +0800

    新增toggleGroup管理组件，处理重复点击及回调

---
+++ base/node/YJToggleGroupManager.ts
@@ -0,0 +1,53 @@
+
+import { _decorator, Component, Node, ToggleContainer, Toggle, CCBoolean } from 'cc';
+import { no } from '../../no';
+const { ccclass, property, menu, requireComponent } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = YJToggleGroupManager
+ * DateTime = Thu Mar 24 2022 16:42:14 GMT+0800 (中国标准时间)
+ * Author = mqsy_yj
+ * FileBasename = YJToggleGroupManager.ts
+ * FileBasenameNoExtension = YJToggleGroupManager
+ * URL = db://assets/NoUi3/base/node/YJToggleGroupManager.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+
+@ccclass('YJToggleGroupManager')
+@menu('NoUi/node/YJToggleGroupManager(ToggleGroup管理)')
+@requireComponent(ToggleContainer)
+export class YJToggleGroupManager extends Component {
+    @property(no.EventHandlerInfo)
+    onToggleChecked: no.EventHandlerInfo[] = [];
+
+    @property({ tooltip: '是否能重复点击，默认false，checked后点击无效' })
+    redo: boolean = false;
+
+    private checkedToggleUuid: string = null;
+
+    onEnable() {
+        let items = this.getComponent(ToggleContainer).toggleItems;
+        for (let i = 0, n = items.length; i < n; i++) {
+            let toggle = items[i];
+            if (toggle.isChecked) {
+                this.a_onCheck(toggle);
+                break;
+            }
+        }
+    }
+
+    public a_onCheck(toggle: Toggle): void {
+        //避免重复点击
+        if (!this.redo && toggle.uuid == this.checkedToggleUuid) return;
+        //onToggleChecked回调方法只有1个，则所有toggle checked共用。
+        //否则按下标调用对应的回调方法
+        this.checkedToggleUuid = toggle.uuid;
+        let i = no.indexOfArray(this.getComponent(ToggleContainer).toggleItems, toggle, 'uuid');
+        (this.onToggleChecked[i] || this.onToggleChecked[0])?.execute(i);
+    }
+
+
+}
+
