5ebebf9b04b2d73efbad61deb7566f0c926ab144
YanJun19820509 <mqsy_yj@163.com>
2022-04-19 12:23:22 +0800

    新增动态创建可缓存子节点的组件

---
+++ fuckui/SetCreateCacheNode.ts
@@ -0,0 +1,60 @@
+
+import { _decorator, Component, Node, instantiate } from 'cc';
+import YJLoadPrefab from '../base/node/YJLoadPrefab';
+import { YJCacheObject } from '../base/YJCacheObject';
+import { YJDataWork } from '../base/YJDataWork';
+import { YJLoadAssets } from '../editor/YJLoadAssets';
+import { no } from '../no';
+import { FuckUi } from './FuckUi';
+const { ccclass, menu, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = SetCreateCacheNode
+ * DateTime = Tue Apr 19 2022 10:17:54 GMT+0800 (中国标准时间)
+ * Author = mqsy_yj
+ * FileBasename = SetCreateCacheNode.ts
+ * FileBasenameNoExtension = SetCreateCacheNode
+ * URL = db://assets/NoUi3/fuckui/SetCreateCacheNode.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+
+@ccclass('SetCreateCacheNode')
+@menu('NoUi/ui/SetCreateCacheNode(动态创建可回收节点:object|array)')
+export class SetCreateCacheNode extends FuckUi {
+    @property({ type: YJLoadPrefab, displayName: '元素预制体' })
+    loadPrefab: YJLoadPrefab = null;
+    @property({ type: Node, displayName: '元素模板' })
+    template: Node = null;
+
+    @property({ type: Node, displayName: '容器' })
+    container: Node = null;
+
+    private recycleType: string;
+
+    protected onDataChange(data: any) {
+        this.setItems([].concat(data));
+    }
+
+    private async setItems(data: any[]) {
+        if (!this.template) {
+            this.template = await this.loadPrefab.loadPrefab();
+            this.template.getComponent(YJLoadAssets)?.load();
+        }
+        if (!this.container) this.container = this.node;
+        if (!this.recycleType) this.recycleType = this.template.getComponent(YJCacheObject).recycleType;
+
+        for (let i = 0, n = data.length; i < n; i++) {
+            if (!data[i]) continue;
+            let item = no.cachePool.reuse<Node>(this.recycleType) || instantiate(this.template);
+            let a = item.getComponent(YJDataWork) || item.getComponentInChildren(YJDataWork);
+            item.active = true;
+            item.parent = this.container;
+            if (a) {
+                a.data = data[i];
+                a.init();
+            }
+        }
+    }
+}
