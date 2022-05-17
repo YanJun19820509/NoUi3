1d91dbbe967aa052b34e546f61d37b1d73060ed7
YanJun19820509 <mqsy_yj@163.com>
2022-05-17 18:18:24 +0800

    spriteframe释放时直接释放其imageasset

--- no.ts
+++ no.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, EventHandler, game, color, Color, Vec2, AnimationClip, Asset, assetManager, AssetManager, AudioClip, director, instantiate, JsonAsset, Material, Prefab, Rect, Size, sp, SpriteAtlas, SpriteFrame, TextAsset, Texture2D, TiledMapAsset, Tween, v2, v3, Vec3, UITransform, tween, UIOpacity, Quat, EventTarget, EffectAsset } from 'cc';
+import { _decorator, Component, Node, EventHandler, game, color, Color, Vec2, AnimationClip, Asset, assetManager, AssetManager, AudioClip, director, instantiate, JsonAsset, Material, Prefab, Rect, Size, sp, SpriteAtlas, SpriteFrame, TextAsset, Texture2D, TiledMapAsset, Tween, v2, v3, Vec3, UITransform, tween, UIOpacity, Quat, EventTarget, EffectAsset, ImageAsset } from 'cc';
 import { EDITOR, WECHAT } from 'cc/env';
 import { AssetInfo } from '../../extensions/auto-create-prefab/@types/packages/asset-db/@types/public';
 
@@ -1975,7 +1975,13 @@ export namespace no {
          */
         public release(asset: Asset | string, force = false): void {
             if (!asset) return;
-            if (typeof asset == 'string') asset = assetManager.assets.get(asset);
+            if (asset instanceof SpriteFrame) {
+                asset = asset._uuid;
+            }
+            if (typeof asset == 'string') {
+                asset = asset.split('@')[0];
+                asset = assetManager.assets.get(asset);
+            }
             if (!asset) return;
             if (force) assetManager.releaseAsset(asset);
             else asset.decRef();
