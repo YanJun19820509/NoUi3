5488623ba3c82843c4fa4201e9c439559a395a19
YJ <mqsy_yj@163.com>
2022-06-20 19:38:27 +0800

    通过给类型定义别名，解决循环引用的问题

---
+++ types.ts
@@ -0,0 +1,18 @@
+
+import { _decorator, Component, Node } from 'cc';
+import { FuckUi } from './fuckui/FuckUi';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = types
+ * DateTime = Mon Jun 20 2022 15:06:43 GMT+0800 (中国标准时间)
+ * Author = mqsy_yj
+ * FileBasename = types.ts
+ * FileBasenameNoExtension = types
+ * URL = db://assets/NoUi3/types.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+
+export type _FuckUi = FuckUi;
