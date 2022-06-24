38cff2cb44b8f396ea87dd280ff684c2d9630007
YJ <mqsy_yj@163.com>
2022-06-24 17:48:54 +0800

    新增clone目标节点组件到当前节点的组件

---
+++ base/YJCloneComponent.ts
@@ -0,0 +1,64 @@
+
+import { _decorator, Component, Node, js, UITransform } from 'cc';
+import { EDITOR } from 'cc/env';
+const { ccclass, property, executeInEditMode } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = YJCloneComponent
+ * DateTime = Fri Jun 24 2022 16:35:58 GMT+0800 (中国标准时间)
+ * Author = mqsy_yj
+ * FileBasename = YJCloneComponent.ts
+ * FileBasenameNoExtension = YJCloneComponent
+ * URL = db://assets/NoUi3/base/YJCloneComponent.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+/**
+ * 节点组件clone，将target节点上node属性和enabled的组件clone到当前节点上
+ */
+@ccclass('YJCloneComponent')
+@executeInEditMode()
+export class YJCloneComponent extends Component {
+    @property(Node)
+    target: Node = null;
+
+    update() {
+        if (!EDITOR) return;
+        if (!this.target) return;
+        this.setNodeProperties();
+        let comps = this.target.getComponents(Component);
+        comps.forEach(comp => {
+            if (!comp.enabled) return;
+            this.addComp(comp);
+        });
+        this.target = null;
+    }
+
+    private setNodeProperties() {
+        this.node.position = this.target.position;
+        this.node.rotation = this.target.rotation;
+        this.node.scale = this.target.scale;
+        this.node.layer = this.target.layer;
+    }
+
+    private addComp(comp: Component) {
+        let name = js.getClassName(comp);
+        let props = this.getProperties(name);
+        let a = this.node.getComponent(name) || this.node.addComponent(name);
+        props.forEach(p => {
+            a[p] = comp[p];
+        });
+    }
+
+    private getProperties(className: string): string[] {
+        let props: string[] = js.getClassByName(className)['__props__'];
+        // console.log(props)
+        let a: string[] = [];
+        props.forEach(p => {
+            if (p == 'node' || p.indexOf('_') == 0) return;
+            a[a.length] = p;
+        });
+        return a;
+    }
+}
