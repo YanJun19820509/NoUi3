e6677935575feae85ab6a98fb78111fda0591d4c
YJ <mqsy_yj@163.com>
2022-08-04 17:43:56 +0800

    新增监听目标节点scale改变组件

---
+++ base/node/YJWatchTargetNodeScaleChange.ts
@@ -0,0 +1,43 @@
+
+import { _decorator, Component, Node, math, v3 } from 'cc';
+import { no } from '../../no';
+import { YJNodeTarget } from './YJNodeTarget';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = YJWatchTargetNodeScaleChange
+ * DateTime = Thu Aug 04 2022 16:29:53 GMT+0800 (中国标准时间)
+ * Author = mqsy_yj
+ * FileBasename = YJWatchTargetNodeScaleChange.ts
+ * FileBasenameNoExtension = YJWatchTargetNodeScaleChange
+ * URL = db://assets/NoUi3/base/node/YJWatchTargetNodeScaleChange.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+//监听目标节点scale改变
+@ccclass('YJWatchTargetNodeScaleChange')
+export class YJWatchTargetNodeScaleChange extends Component {
+    @property
+    targetType: string = '';
+    @property(no.EventHandlerInfo)
+    onChange: no.EventHandlerInfo[] = [];
+
+    private _targetScale: math.Vec3;
+
+    update() {
+        let target = no.nodeTargetManager.get<YJNodeTarget>(this.targetType);
+        if (target && target.isValid && target.node && target.node.isValid) {
+            if (!this._targetScale) {
+                this._targetScale = v3();
+                target.node.getScale(this._targetScale);
+            } else {
+                let scale = target.node.getScale();
+                if (!this._targetScale.equals(scale)) {
+                    this._targetScale = scale;
+                    no.EventHandlerInfo.execute(this.onChange, this._targetScale);
+                }
+            }
+        }
+    }
+}
