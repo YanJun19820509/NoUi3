51772b474f7de1be5eb9ecec3bafd1df9c1455b3
YJ <mqsy_yj@163.com>
2022-08-17 19:22:58 +0800

    新增showforjsb组件

---
+++ base/node/YJShowForJSB.ts
@@ -0,0 +1,23 @@
+
+import { _decorator, Component, Node } from 'cc';
+import { JSB } from 'cc/env';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = YJShowForJSB
+ * DateTime = Wed Aug 17 2022 18:33:07 GMT+0800 (中国标准时间)
+ * Author = mqsy_yj
+ * FileBasename = YJShowForJSB.ts
+ * FileBasenameNoExtension = YJShowForJSB
+ * URL = db://assets/NoUi3/base/node/YJShowForJSB.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+
+@ccclass('YJShowForJSB')
+export class YJShowForJSB extends Component {
+    onLoad() {
+        !JSB && this.node.destroy();
+    }
+}
