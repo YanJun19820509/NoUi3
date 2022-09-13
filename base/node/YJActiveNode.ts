fc77a4dbefbba1fb0d2397edbd6e4da64b5b7138
YJ <mqsy_yj@163.com>
2022-09-13 11:37:14 +0800

    新增节点激活组件

---
+++ base/node/YJActiveNode.ts
@@ -0,0 +1,30 @@
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = YJActiveNode
+ * DateTime = Tue Sep 13 2022 11:23:21 GMT+0800 (中国标准时间)
+ * Author = mqsy_yj
+ * FileBasename = YJActiveNode.ts
+ * FileBasenameNoExtension = YJActiveNode
+ * URL = db://assets/NoUi3/base/node/YJActiveNode.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+
+@ccclass('YJActiveNode')
+export class YJActiveNode extends Component {
+    public a_active(): void {
+        this.setActive(true);
+    }
+
+    public a_inactive(): void {
+        this.setActive(false);
+    }
+
+    private setActive(v: boolean) {
+        this.node.active = v;
+    }
+}
