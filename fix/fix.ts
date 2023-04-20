c870c8f687ffe98bc291a3419622c7a948fa170f
YJ <mqsy_yj@163.com>
2023-04-20 20:31:53 +0800

    fix

--- fix/fix.ts
+++ fix/fix.ts
@@ -15,8 +15,17 @@ const { ccclass, property } = _decorator;
  */
 const _getStencilStage = StencilManager.prototype.getStencilStage;
 js.mixin(StencilManager.prototype, {
-    getStencilStage(stage: any, mat?: Material) {
+    getStencilStage(stage: any, mat?: any) {
         if (!mat || !mat.passes) mat = null;
         return _getStencilStage.call(this, stage, mat);
+    }
+});
+
+
+const _commitIA: Function = cc['internal']?.['Batcher2D']['prototype']['commitIA'];
+js.mixin(cc['internal']?.['Batcher2D']['prototype'], {
+    commitIA(renderComp: any, ia: any, tex?: any, mat?: any, transform?: Node) {
+        if (!tex || !tex.getGFXSampler()) tex = null;
+        return _commitIA.call(this, renderComp, ia, tex, mat, transform);
     }
 });
