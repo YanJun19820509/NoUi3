d603fab12dd2c49f3351b60a132e1b4221523202
YJ <mqsy_yj@163.com>
2023-07-11 17:31:45 +0800

    fix

---
+++ base/YJDataMap.ts
@@ -0,0 +1,57 @@
+import { YJDataWork } from '../../NoUi3/base/YJDataWork';
+import { ccclass, js, property } from '../yj';
+import { YJGameData } from './YJGameData';
+
+/**
+ * 数据映射
+ * Author mqsy_yj
+ * DateTime Tue Jul 11 2023 16:28:37 GMT+0800 (中国标准时间)
+ *
+ */
+@ccclass('YJDataMapInfo')
+export class YJDataMapInfo {
+    @property({ displayName: '数据key', tooltip: '如果对应多个key用,分隔，将解析为kv结构' })
+    dataKeys: string = '';
+    @property
+    uiKey: string = '';
+
+    public getData(dataSource: YJGameData): any {
+        const keys = this.dataKeys.split(',');
+        if (keys.length == 1) return dataSource.get(keys[0]);
+        let a: any = {};
+        keys.forEach(k => {
+            a[k] = dataSource.get(k);
+        });
+        return a;
+    }
+}
+@ccclass('YJDataMap')
+export class YJDataMap extends YJDataWork {
+    @property({ displayName: '数据源', tooltip: '数据对象类名，继承YJGameData' })
+    dataSourceClassName: string = '';
+    @property({ type: YJDataMapInfo })
+    keyMaps: YJDataMapInfo[] = [];
+
+    private _dataSource: YJGameData;
+
+    protected afterInit() {
+        if (!this._dataSource) {
+            const c = js.getClassByName(this.dataSourceClassName) as (typeof YJGameData);
+            this._dataSource = c.instance();
+            if (this._dataSource) {
+                this.update = (dt: number) => {
+                    super.update?.(dt);
+                    if (this._dataSource.checkStateChange(this)) this.syncWithDataSource();
+                };
+            }
+        }
+        this.syncWithDataSource();
+    }
+
+    protected syncWithDataSource() {
+        if (!this._dataSource) return;
+        this.keyMaps.forEach(km => {
+            this.setValue(km.uiKey, km.getData(this._dataSource));
+        });
+    }
+}
---
+++ base/YJGameData.ts
@@ -0,0 +1,28 @@
+import { ccclass } from '../../NoUi3/yj';
+import { no } from '../no';
+
+/**
+ * 游戏数据基类
+ * Author mqsy_yj
+ * DateTime Tue Jul 11 2023 16:40:10 GMT+0800 (中国标准时间)
+ *
+ */
+
+@ccclass('YJGameData')
+export class YJGameData extends no.Data {
+    private static _ins: any;
+    private _state: no.State;
+
+    public static instance(): any {
+        if (!this._ins) {
+            this._ins = new this();
+            this._ins._state = new no.State();
+        }
+        return this._ins;
+    }
+
+    public checkStateChange(target: any): boolean {
+        return this._state.check('data_change', target).state;
+    }
+
+}
