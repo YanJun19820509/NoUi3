1db6beef7ea4cda8ceede86652254edbb698df03
YJ <mqsy_yj@163.com>
2023-12-18 20:23:01 +0800

    fix

--- base/YJGameState.ts
+++ base/YJGameState.ts
@@ -26,9 +26,6 @@ export class YJGameState extends Component {
     @property({ type: no.EventHandlerInfo, displayName: '内存不足时回调' })
     onLowMemoryCalls: no.EventHandlerInfo[] = [];
 
-    @property({ displayName: '进入后台多长时间执行回到前台回调(秒)' })
-    duration: number = 30;
-
     @property({ displayName: '游戏退出的事件', readonly: true })
     event_end: string = 'game_state_event_end';
 
@@ -40,8 +37,6 @@ export class YJGameState extends Component {
 
     @property({ type: no.EventHandlerInfo, displayName: '游戏重启时回调', visible() { return !this.isEngineRestart; } })
     onGameRestart: no.EventHandlerInfo[] = [];
-
-    private time: number;
 
     onLoad() {
         game.on(Game.EVENT_HIDE, this.onHide, this);
@@ -56,13 +51,11 @@ export class YJGameState extends Component {
     }
 
     private onHide(): void {
-        this.time = no.timestamp();
         no.EventHandlerInfo.execute(this.onHideCalls);
     }
 
     private onShow(): void {
-        if (no.timestamp() - this.time >= this.duration)
-            no.EventHandlerInfo.execute(this.onShowCalls);
+        no.EventHandlerInfo.execute(this.onShowCalls);
     }
 
     private onLowMemory() {
