5b14f7a66e85a964bfb904b4a17bc0341d98cf82
yanjun <mqsy_yj@163.com>
2024-09-26 18:43:10 +0800

    fix

--- base/YJFitScreen.ts
+++ base/YJFitScreen.ts
@@ -23,94 +23,37 @@ export class YJFitScreen extends Component {
 
     /**显示区域矢量值 */
     public static rectV: Rect = new Rect(0, 0, 1, 1);
-    /**显示区域，相对于屏幕 */
-    public static rect: Rect = new Rect();
 
     public static policy: number = ResolutionPolicy.NO_BORDER;
 
     onLoad() {
-        this.adapt();
         view.on("canvas-resize", () => {
             no.log('window resize');
             this.adapt();
         }, this);
+        this.scheduleOnce(this.adapt, 1);
     }
 
     private adapt() {
-        let size = view.getVisibleSize();
         if (!this.enabled) {
-            YJFitScreen.rect = new Rect(0, 0, size.width, size.height);
             return;
         }
-        const dsize = view.getDesignResolutionSize(),
-            ss = size.width / size.height,
-            dss = dsize.width / dsize.height;
-        let policyType: number, rect = new Rect(0, 0, 1, 1), w = dsize.width, h = dsize.height, s = 1;
-        // if (dss < 1) {//竖屏
-        if (ss > dss) policyType = ResolutionPolicy.FIXED_HEIGHT;
+        const dir = no.deviceOrientation(),
+            dsize = view.getDesignResolutionSize();
+        let policyType: number;
+        if (dir == 0 && dsize.height > dsize.width) policyType = ResolutionPolicy.FIXED_HEIGHT;
         else policyType = ResolutionPolicy.FIXED_WIDTH;
-        // } else {//横屏
-        //     if (ss < dss) policyType = ResolutionPolicy.FIXED_WIDTH;
-        //     else policyType = ResolutionPolicy.FIXED_HEIGHT;
-        // }
         YJFitScreen.policy = policyType;
         view.setResolutionPolicy(policyType)
-        size = view.getVisibleSize();
         if (policyType == ResolutionPolicy.FIXED_HEIGHT) {
-            // s = dsize.width / size.width;
-            // rect.xMin = .5 - s / 2;
-            // rect.xMax = .5 + s / 2;
             no.size(this.canvas.node, dsize);
             no.size(this.node, dsize);
             //canvas  锚点虽然是0.5,0.5  但实际是0,0   需要重新设置坐标定位
             no.position(this.canvas.node, v3(dsize.width / 2, dsize.height / 2));
-            // this.camera.rect = rect;
         } else if (policyType == ResolutionPolicy.FIXED_WIDTH) {
-            // no.size(this.canvas.node, dsize);
             no.size(this.node, no.size(this.canvas.node));
             this.canvas.alignCanvasWithScreen = true;
         }
-        // no.scale(this.canvas.node, v3(s, s, 1));
-        // view.setDesignResolutionSize(w * s, h * s, policyType);
-        // no.scale(this.canvas.node, v3(s, s, 1));
-        // no.size(this.canvas.node, view.getDesignResolutionSize());
-
-
-
-        // if ((dss < 1 && policyType == ResolutionPolicy.FIXED_HEIGHT) || (dss > 1 && policyType == ResolutionPolicy.FIXED_WIDTH)) {
-        //     const result = view.getResolutionPolicy()['_contentStrategy']['_result'],
-        //         scale: number[] = result.scale,
-        //         w = dsize.width * scale[0],
-        //         h = dsize.height * scale[1];
-        //     rect.width = no.divide(w, size.width);
-        //     rect.height = no.divide(h, size.height);
-        //     rect.x = 0.5 - rect.width / 2;
-        //     rect.y = 0.5 - rect.height / 2;
-
-        //     this.camera.rect = rect;
-        //     // YJFitScreen.rectV = rect;
-        //     // YJFitScreen.rect = new Rect(rect.x * size.width, rect.y * size.height, w, h);
-        // }
-
-        // const widget = this.canvas.getComponent(Widget) || this.canvas.addComponent(Widget);
-        // if (policyType == ResolutionPolicy.FIXED_WIDTH) {
-        //     widget.isAlignTop = true;
-        //     widget.isAlignBottom = true;
-        //     widget.isAlignLeft = true;
-        //     widget.isAlignRight = true;
-        //     widget.top = 0;
-        //     widget.bottom = 0;
-        //     widget.left = 0;
-        //     widget.right = 0;
-        // } else if (policyType == ResolutionPolicy.FIXED_HEIGHT) {
-        //     widget.isAlignTop = true;
-        //     widget.isAlignBottom = true;
-        //     widget.isAlignLeft = false;
-        //     widget.isAlignRight = false;
-        //     widget.top = 0;
-        //     widget.bottom = 0;
-        // }
-        // widget.updateAlignment();
     }
 
     public static fitTouchPoint(touch: Touch): Vec2 {
