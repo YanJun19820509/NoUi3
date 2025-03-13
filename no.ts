f458b7f12318792a61b16c787c4fcd923fe58df8
yanjun <mqsy_yj@163.com>
2025-03-13 17:37:32 +0800

    新增循环索引函数circleIndex，用于在指定范围内循环返回索引值

--- no.ts
+++ no.ts
@@ -1862,6 +1862,19 @@ export namespace no {
         return v;
     }
 
+    /**
+     * 循环索引, 当n < min时返回max，当n > max时返回min，否则返回n
+     * @param n 
+     * @param min 
+     * @param max 
+     * @returns 
+     */
+    export function circleIndex(n: number, min: number, max: number): number {
+        if (n < min) return max;
+        if (n > max) return min;
+        return n;
+    }
+
     function eIndex(n: number): number {
         let s = n.toString().toLowerCase();
         let a = s.split('e');
