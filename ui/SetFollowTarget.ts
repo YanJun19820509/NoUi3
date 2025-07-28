401d5f551a112501ba51b3377f993bba7e09a0d2
yanjun <mqsy_yj@163.com>
2025-07-28 20:56:25 +0800

    新增跟随组件

---
+++ ui/SetFollowTarget.ts
@@ -0,0 +1,63 @@
+import { ccclass, Node, property, Vec3 } from 'hackUi/yj';
+import { HackUi } from 'hackUi/ui/HackUi';
+import { no } from '../no';
+import { YJNodeTarget } from '../base/node/YJNodeTarget';
+
+/**
+ * 跟随目标,状态同步
+ * Author mqsy_yj
+ * DateTime Mon Jul 28 2025 17:20:35 GMT+0800 (中国标准时间)
+ *
+ */
+
+@ccclass('SetFollowTarget')
+export class SetFollowTarget extends HackUi {
+    @property
+    syncPos: boolean = true;
+    @property({ visible() { return this.syncPos } })
+    posOffset: Vec3 = new Vec3();
+    @property
+    syncScale: boolean = true;
+    @property({ visible() { return this.syncScale } })
+    reserve: boolean = false;
+
+    private _target: Node = null;
+    private _selfScale: Vec3 = null;
+
+    protected onDataChange(data: any) {
+        if (data == 'null') {
+            this.clearTarget();
+        } else {
+            this.initTarget(data);
+        }
+    }
+
+    private clearTarget() {
+        if (this._target) {
+            this._target.off(Node.EventType.TRANSFORM_CHANGED, this.onTargetTransformChanged, this);
+            this._target = null;
+        }
+    }
+
+    private initTarget(targetName: string) {
+        this._target = no.nodeTargetManager.get<YJNodeTarget>(targetName).node;
+        this._target.on(Node.EventType.TRANSFORM_CHANGED, this.onTargetTransformChanged, this);
+        this.onTargetTransformChanged();
+    }
+
+    private onTargetTransformChanged() {
+        if (this.syncPos) {
+            const pos = this._target.position;
+            this.node.setPosition(pos.x + this.posOffset.x, pos.y + this.posOffset.y, pos.z + this.posOffset.z);
+        }
+        if (this.syncScale) {
+            if (!this._selfScale) this._selfScale = this.node.scale.clone();
+            const scale = this._target.scale;
+            let a = 1;
+            if (this.reserve) {
+                a = -1;
+            }
+            this.node.setScale(scale.x * a * this._selfScale.x, scale.y * a * this._selfScale.y, scale.z * a * this._selfScale.z);
+        }
+    }
+}
