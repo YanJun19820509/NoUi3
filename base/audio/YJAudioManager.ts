d5d50d538d2b332f04d655fbac77b7bfd8df66c5
yanjun <mqsy_yj@163.com>
2025-08-22 19:06:25 +0800

    在YJAudioManager类中优化音效的暂停和恢复逻辑，使用pause和play方法替代volume属性，以更准确地控制音效播放状态。

--- base/audio/YJAudioManager.ts
+++ base/audio/YJAudioManager.ts
@@ -593,13 +593,13 @@ export class YJAudioManager extends Component {
      * 暂停非永久音效播放
      */
     public pause(): void {
-        this.audioSource.volume = 0;
+        this.audioSource.pause();
     }
     /**
      * 恢复非永久音效播放
      */
     public resume(): void {
-        this.audioSource.volume = 1;
+        this.audioSource.play();
     }
     /**
      * 停止所有非永久音效播放
@@ -615,14 +615,14 @@ export class YJAudioManager extends Component {
     public pauseAll(): void {
         this.pause();
         if (this.audioSourceForever) {
-            this.audioSourceForever.volume = 0;
+            this.audioSourceForever.pause();
         }
     }
 
     public resumeAll(): void {
         this.resume();
         if (this.audioSourceForever) {
-            this.audioSourceForever.volume = 1;
+            this.audioSourceForever.play();
         }
     }
 
