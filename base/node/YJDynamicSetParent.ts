168c769a821a0ab79e8dfad895d2263e5899f23d
yanjun <mqsy_yj@163.com>
2025-11-04 19:04:32 +0800

    fix

--- base/node/YJDynamicSetParent.ts
+++ base/node/YJDynamicSetParent.ts
@@ -14,22 +14,50 @@ export class YJDynamicSetParent extends Component {
     newParent: Node = null;
 
     private _oldParent: Node = null;
+    private _oldParentPos: Vec3;
     private _oldPos: Vec3;
 
-    onLoad() {
-        this._oldParent = this.node.parent;
-    }
     protected onEnable(): void {
         if (this.newParent) {
-            this._oldPos = this.node.position.clone();
-            const newPos = no.nodePositionInOtherNode(this.node, this.newParent);
-            this.node.parent = this.newParent;
-            this.node.setPosition(newPos);
+            this.scheduleOnce(this.changeParent, 1);
         }
     }
 
     protected onDisable(): void {
+        // this.resetParent();
+    }
+
+    private changeParent() {
+        if (!this._oldParent) {
+            this._oldParent = this.node.parent;
+            this._oldParentPos = this._oldParent.position.clone();
+            this._oldParent.on(Node.EventType.TRANSFORM_CHANGED, this.onTransformChanged, this);
+        }
+        this._oldPos = this.node.position.clone();
+        const newPos = no.nodePositionInOtherNode(this.node, this.newParent);
+        this.node.parent = this.newParent;
+        this.node.setPosition(newPos);
+    }
+
+    private resetParent() {
         this.node.parent = this._oldParent;
         this.node.setPosition(this._oldPos);
+        this._oldParent.off(Node.EventType.TRANSFORM_CHANGED, this.onTransformChanged, this);
+    }
+
+    private onTransformChanged(type: number) {
+        if (type & Node.TransformBit.POSITION) {
+            let pos = no.position(this._oldParent);
+            let x = pos.x - this._oldParentPos.x;
+            let y = pos.y - this._oldParentPos.y;
+            let selfPos = this.node.position;
+            this.node.setPosition(selfPos.x + x, selfPos.y + y);
+        }
+        if (type & Node.TransformBit.ROTATION) {
+            no.rotation(this.node, no.rotation(this._oldParent));
+        }
+        if (type & Node.TransformBit.SCALE) {
+            no.scale(this.node, no.scale(this._oldParent));
+        }
     }
 }
